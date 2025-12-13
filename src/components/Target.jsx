ee819e342650578bd3ed40043b23154b9b89485a
bhrgvbhrgv <bhargavdas21th@gmail.com>
2025-12-13 19:44:15 +0530

    fixed changes

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -6,7 +6,7 @@ import gsap from 'gsap';
 const Target = (props) => {
   const targetRef = useRef();
   const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
+    'https://rmkoeoijutjjekndrgwz.supabase.co/storage/v1/object/public/models/target-stand/scene.gltf',
   );
 
   useGSAP(() => {
